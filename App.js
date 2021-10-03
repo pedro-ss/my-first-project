88e5e8a3b5a5d9fc4f9551accedfe0b80fad917c
Pedro Souza Silva <pedross.tool@outlook.com>
2021-10-03 15:47:16 -0300

    prática para a disciplina de disponsitivos móveis.

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Olá meu nome é Pedro</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Click me"
+        color="#841584"
+        accessibilityLabel="Se chegou aqui, procurou ler meu código."
+        onPress={() => Alert.alert('Sou aluno de ADS do IFPE')}
+      />
     </View>
   );
 }
